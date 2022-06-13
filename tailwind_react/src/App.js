f468914770b68387f1d180ab72e9518e1f394269
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-13 12:35:38 +0600

    clip path with extended content

--- tailwind_react/src/App.js
+++ tailwind_react/src/App.js
@@ -9,12 +9,15 @@ function App() {
         </h2>
       </div>
 
-      <div className="w-[50%] mt-5 mx-auto rounded-lg p-20 before:rounded-lg custom_clip_path">
+      {/* clip path  */}
+      <div className="w-[30%] mt-5 mx-auto rounded-lg p-20 bg-red-300 before:bg-indigo-400 before:rounded-lg custom_clip_path">
         <h2 className="text-center text-white text-2xl titl">clip path</h2>
-        <h2 className="text-center text-white text-2xl titl">clip path</h2>
-        <h2 className="text-center text-white text-2xl titl">clip path</h2>
-        <h2 className="text-center text-white text-2xl titl">clip path</h2>
-        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+      </div>
+
+      <div className="w-[30%] mt-5 mx-auto rounded-lg p-2 py-20 bg-green-300 before:bg-indigo-400 before:rounded-lg custom_clip_path2">
+        <p className="text-center text-white text-2xl titl">
+          clip path with extended
+        </p>
       </div>
     </>
   );
