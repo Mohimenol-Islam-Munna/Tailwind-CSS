9686008fdebfec330b82da16cf42aaef14d4d31d
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-13 12:17:04 +0600

    clip path

--- tailwind_react/src/App.js
+++ tailwind_react/src/App.js
@@ -1,13 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="w-52 mx-auto">
-      <h2 className="text-center text-white bg-blue-600 py-6 sm:text-left lg:text-right ">
-        React Tailwind
-      </h2>
-    </div>
+    <>
+      <div className="bg-green-500 p-8">
+        <h2 className="text-center text-white font-bold text-2xl">
+          React Tailwind
+        </h2>
+      </div>
+
+      <div className="w-[50%] mt-5 mx-auto rounded-lg p-20 before:rounded-lg custom_clip_path">
+        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+        <h2 className="text-center text-white text-2xl titl">clip path</h2>
+      </div>
+    </>
   );
 }
 
