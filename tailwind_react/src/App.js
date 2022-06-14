3472b62b28a19dcdf5bae969c2cde0c8d4649cfb
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-14 16:56:30 +0600

    transition on custom checked box

--- tailwind_react/src/App.js
+++ tailwind_react/src/App.js
@@ -165,7 +165,7 @@ function App() {
             />
             <label
               htmlFor="check"
-              className="group block w-full h-[35px] border-2 border-yellow-900 rounded-full ring-4 before:content-[''] before:bg-green-400 relative z-10 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:rounded-full before:-z-10 before:translate-x-[-50%] overflow-hidden peer-checked:before:translate-x-[50%]"
+              className="group block w-full h-[35px] border-2 border-yellow-900 rounded-full ring-4 before:content-[''] before:bg-green-400 relative z-10 before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:rounded-full before:-z-10 before:translate-x-[-40%] overflow-hidden peer-checked:before:translate-x-[40%] before:transition-all before:duration-500"
             ></label>
           </div>
         </div>
